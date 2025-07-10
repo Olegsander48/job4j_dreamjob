002fbf6ee618178f72a5cd5c9d99aad23acebbaf
Aleksandr Prigodich <prigodichaleks@gmail.com>
2025-07-10 10:04:15 +0300

    2. HttpSession [#504864]

---
+++ src/main/java/ru/job4j/dreamjob/controller/GlobalUserAdvice.java
@@ -0,0 +1,19 @@
+package ru.job4j.dreamjob.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import ru.job4j.dreamjob.model.User;
+
+@ControllerAdvice
+public class GlobalUserAdvice {
+    @ModelAttribute("user")
+    public User addUserToModel(HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            user = new User();
+            user.setName("Гость");
+        }
+        return user;
+    }
+}
